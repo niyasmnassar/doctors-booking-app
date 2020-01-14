b3e2f78ae7444fb450f095e2dc8b955b5cae0d36
Niyas <you@example.com>
2020-01-14 18:24:31 +0530

    directory added

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + "/dist/doctors-booking-app"));
 app.listen(process.env.PORT || 8080);
 app.get('/*', function(req, res){
     res.sendFile(path.join(__dirname + "/dist/doctors-booking-app/index.html"));
