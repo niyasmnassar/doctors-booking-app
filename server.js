cabd5d13ae0c8dc1c34b168ac9dc9a64adf73bbb
Niyas <you@example.com>
2020-01-14 18:23:15 +0530

    folder added

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 8080);
 app.get('/*', function(req, res){
-    res.sendFile(path.join(__dirname + '/dist/index.html'))
+    res.sendFile(path.join(__dirname + "/dist/doctors-booking-app/index.html"));
 })
 console.log('Console Listening!');
 
